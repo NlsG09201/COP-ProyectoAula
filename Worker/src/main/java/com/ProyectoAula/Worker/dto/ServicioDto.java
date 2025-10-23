ed1324fdeb360138251fcb12db9db3c32771b391
Your Name <nelsondavid1954@gmail.com>
2025-10-23 02:02:23 -0500

    V0.04

--- Worker/src/main/java/com/ProyectoAula/Worker/dto/ServicioDto.java
+++ Worker/src/main/java/com/ProyectoAula/Worker/dto/ServicioDto.java
@@ -3,4 +3,9 @@ package com.ProyectoAula.Worker.dto;
 public record ServicioDto(
         Long idServicio,
         String tipoServicio
-) {}
+) {
+
+    public Object nombre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nombre'");
+    }}
